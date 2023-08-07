ac5a2717fac19f31550d74a4d2f9707cbe0a00fe
shraddha-sadavarte <shraddhasadavarte24@gmail.com>
2023-08-07 11:29:38 -0700

    practice on 7/8/23

--- advance js/app.js
+++ advance js/app.js
@@ -49,3 +49,101 @@ var ans = (function(){
         // }
     }
 })();
+
+//prototype: when we create an object we have a prototype inherited in that object, this contains many helper methods and properties
+//prototypal inheritance: We can inherit another object properties by using prototype is called prototypal inheirtance
+var human = {
+    canFly: false,
+    canTalk: true,
+    canWalk: true,
+    haveEmotions: true
+};
+
+var developer = {
+    canMakeWebsites: true,
+    canBuildApplications: true
+};
+
+//inherit one object in another object
+developer.__proto__ = human;
+
+//this keyword ki value object, method and function ke andar alag alg hoti hain 
+//this keyword in global scope
+console.log(this); //it returns window object under global scope
+
+//this keyword in function scope
+function a(){
+    console.log(this); //it returns window object also under function scope
+}
+a();
+
+//this keyword in method scope
+var obj = {
+    name:'yash',
+    someMethod: function(){
+        console.log(this); //it returns object obj in method obj
+    }
+}
+obj.someMethod();
+
+//this keyword value under event listener
+var btn = document.querySelector("button");
+btn.addEventListener("click", function xyz(){
+    console.log(this); //it return btn 
+    console.log(this.style.color = "violet"); //it return btn when click on it color changes to violet
+})
+
+//agar hume this ki value window se object change krni hain tb hum call, apply and bind ka use krte hain
+//call()
+function abcd(){
+    console.log(this) //.name will return the name value i.e "yash"
+}
+
+var g = {
+    name:'yash'
+}
+
+abcd.call(g); //call function with call method and passing the object....it will change this keyword value from window to object
+
+//apply()
+//apply is just like call but it takes two parameter one is object and another one is array.....array is for parameter value passing to the function
+function y(val1, val2){
+    console.log(this, val1, val2); //it will return obj and 24 and 17 parameter value we passed
+}
+
+var r = {
+    name:'shraddha'
+}
+
+y.apply(r, [24,17]);
+
+//bind()
+//bind doesn`t return anything as soon as we keep bind in one variable because bind returns a binded function of our function and object
+function n(){
+    console.log(this);
+}
+
+var o = {
+    age: 12
+}
+
+var bindedFunc = n.bind(o);
+bindedFunc(); //calling a binded function which is bind using .bind() method
+
+//pure functions has 2 features:
+ //1) It always return same output with same input
+ //2) It does not change/update the global variables
+function pure(val){
+    return val*2; //it always returns same value 
+}
+var a = pure(2);
+var b = pure(2);
+console.log(a,b);
+
+//impure functions does not return the same value and it also change the global variable values
+function impure(val){
+    return Math.random() *  val; //does not return the same value
+}
+var ans1 = impure(2);
+var ans2 = impure(2);
+console.log(ans1, ans2);
