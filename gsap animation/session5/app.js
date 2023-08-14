3e9a07296d684ed172a957ff5338dae5dc4ece1e
shraddha-sadavarte <shraddhasadavarte24@gmail.com>
2023-08-14 11:21:08 -0700

    loader added in 5th session website

--- gsap animation/session5/app.js
+++ gsap animation/session5/app.js
@@ -1,3 +1,32 @@
+var tl = gsap.timeline();
+
+function time(){
+    var a = 0
+    setInterval(function(){
+        a = a + (Math.floor(Math.random()*15))
+        if(a<100){
+            document.querySelector(".loader h1").innerHTML = a+"%"
+        } else{
+            a = 100
+            document.querySelector(".loader h1").innerHTML = a+"%"
+        }
+    }, 150)
+}
+
+tl.to(".loader h1", {
+    scale: 1.5,
+    delay: 0.5,
+    duration: 1,
+    onStart: time()
+})
+
+tl.to(".loader", {
+    top: "-100vh",
+    delay: 0.5,
+    duration: 1.3
+})
+
+
 gsap.to(".page1 h1" ,{
     transform: "translateX(-100%)",
     fontWeight: "100",
